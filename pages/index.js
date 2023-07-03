2e9ca2ead4ebca35cb7148406c60d9be4f0fd167
SiimonStark <siimonstark@gmail.com>
2023-07-02 23:49:13 -0700

    Updated build to success

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Head from 'next/head';
+import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { ContactWidget } from '../components/ContactWidget';

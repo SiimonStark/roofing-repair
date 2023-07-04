4aaf2b413469528f5f54f39e25becadc3e8e7444
SiimonStark <siimonstark@gmail.com>
2023-07-04 03:00:04 -0700

    Lost all work after rebase fiasco

--- pages/about_us.js
+++ pages/about_us.js
@@ -1,7 +1,41 @@
+import styles from '../styles/Services.module.css';
+
 const about_us = () => {
     return (
-        <div className="about_us">
-            <p>About Us</p>
+        <div className={styles.about_us}>
+            <section class={styles.hero}>
+                <img src="/finished_jobs/bigfootroof_flag.JPG" />
+            </section>
+            <article>
+                <h4>A Tale of Two Brothers</h4>
+                <p>This little Bigfoot family tale begins in the hills of a quaint little river town on the Upper Rogue. The brothers began their life as little rambunctious wildling cubs as they roamed the outskirts of the towns in the Upper Rogue community. You may have seen them passing through your backyard as they snuck into their favorite fishing, mushroom and berry picking spots. As the years passed and the cubs grew older, they began to wonder how the forests of the rest of the world compared to their own, and in the mid “noughties” the naughty young adult sasquatch brothers chose to wander down two different paths and began to explore the world.</p>
+                <p>One brother went deep into the frozen woods of Alaska where he found work in the gold mining industry while the other joined the Army and got himself lost in the woods at Fort Leonard Wood, Missouri. Over 13 years the brothers explored over two thirds of the United States, including Alaska and Hawaii, with a little time in the jungles of Thailand. Those danger noodles are no joke, both the spicy and poisonous ones.</p>
+                <p>As anyone can tell you, being out and about in the wilderness is amazing and sometimes magical. However, there is nothing like the warmth and comfort of your own home. After over a decade of longing for home, the two brothers finally reconnected where they first sprouted their roots in Southern Oregon. Where, they decided to combine their experience and skills to start a Roofing Company and help others protect what is most precious to them… their home.</p>
+                <p>But we’re not just any roofing company! Bigfoot Roofing, LLC is committed to providing our customers with the best quality roof. We also want to leave a BIG footprint on Southern Oregon. Not a Carbon one. Rather, we want to help our communities prosper by providing field trips and youth programs for kids to experience and learn about the outdoors. To help our veterans, nurses and first responders with discounted rates, volunteer services and we are currently in the process of starting a nonprofit to assist low-income Veterans with essential home repairs. So when you buy a roof from Bigfoot Roofing, LLC you’re not just helping a local Veteran Owned business. You’re helping us make a positive impact on Southern Oregon.</p>
+            </article>
+            <h3>Work that we &#x27;re proud of:</h3>
+            <hr />
+            <section class={styles.works}>
+                <img src="https://imgur.com/tRvAKCc.jpg" />
+                <img src="https://i.imgur.com/ajJiXX3.jpg" />
+                <img src="https://imgur.com/l3OXToT.jpg" />
+                <img src="https://imgur.com/i7vOZnm.jpg" />
+                <img src="https://imgur.com/ZOUu7cv.jpg" />
+                <img src="https://imgur.com/xYDWjbk.jpg" />
+                <img src="https://imgur.com/MGUMdG4.jpg" />
+                <img src="https://imgur.com/3PaP3tR.jpg" />
+                <img src="https://imgur.com/UsDq8x4.jpg" />
+                <img src="https://imgur.com/l2CkyoI.jpg" />
+                <img src="https://imgur.com/FmIc6sG.jpg" />
+                <img src="https://imgur.com/8GVTUIt.jpg" />
+                <img src="https://imgur.com/PLGwVaV.jpg" />
+                <img src="https://imgur.com/8lUEtFE.jpg" />
+                <img src="https://imgur.com/PqHytGi.jpg" />
+                <img src="https://imgur.com/aPjRdwM.jpg" />
+                <img src="https://imgur.com/wSrXPut.jpg" />
+                <img src="https://imgur.com/KMON4LP.jpg" />
+                <img src="https://imgur.com/D635pA0.jpg" />
+            </section>
         </div>
     );
 }
